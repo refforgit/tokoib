5009bff37beddd2dcd0ed9f81ede40aec20c9ebb
refforgit <refforgit58@gmail.com>
2018-03-12 11:01:43 +0700

    Update configwwq.php

--- inc/configwwq.php
+++ inc/configwwq.php
@@ -20,3 +20,13 @@ echo "</tr>";
 }
 echo "</table>";
 ?>
+
+<?php	
+	define('db_host','sql10.freemysqlhosting.net');
+	define('db_user','sql10226087');
+	define('db_pass','7EaWKdAZlV');
+	define('db_name','sql10226087');
+	
+	mysql_connect(db_host,db_user,db_pass);
+	mysql_select_db(db_name);
+?>
