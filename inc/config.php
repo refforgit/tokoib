c1343aa16067f7cf9ec080fa8ddce9ac13e07da0
refforgit <refforgit58@gmail.com>
2018-03-12 11:42:41 +0700

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -3,7 +3,7 @@
 $conn = pg_connect("host=ec2-54-235-66-24.compute-1.amazonaws.com port=5432 dbname=d8ig8ica3tatsa user='cfohoabinecfoa'
 password='aec261c1978f6fa27bf7eb4dd625847f7ba7522e2e957dadc1aeb4f6904221f4'");
 //disini nama database saya adalah nama_database
-$result = pg_prepare($conn, "my_query", 'SELECT * FROM mahasiswa');
+$result = pg_prepare($conn, "my_query", 'SELECT * FROM 'berita');
 // disini saya membuat table dengan nama mahasiswa
 $result = pg_execute($conn, "my_query",array());
 echo "<table border='1px'>
@@ -14,8 +14,8 @@ echo "<table border='1px'>
 while ($row = pg_fetch_assoc($result))
 {
 echo "<tr>";
-echo "<td>".$row['nim']."</td>";
-echo "<td>".$row['nama']."</td>";
+echo "<td>".$row['idberita']."</td>";
+echo "<td>".$row['tanggal']."</td>";
 echo "</tr>";
 }
 echo "</table>";
