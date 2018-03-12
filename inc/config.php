5bb28908590edcba9ad3510b64867e0906604132
refforgit <refforgit58@gmail.com>
2018-03-12 12:07:41 +0700

    Update config.php

--- inc/config.php
+++ inc/config.php
@@ -1,7 +1,7 @@
 <?php
 // script untuk memanggil fungsi php pg_connect untuk koneksi ke postgresql
 $conn = pg_connect("host=ec2-54-235-66-24.compute-1.amazonaws.com port=5432 dbname=d8ig8ica3tatsa user='cfohoabinecfoa'
-password='dd'");
+password='aec261c1978f6fa27bf7eb4dd625847f7ba7522e2e957dadc1aeb4f6904221f4'");
 //disini nama database saya adalah nama_database
 $result = pg_prepare($conn, "my_query", 'SELECT * FROM berita');
 // disini saya membuat table dengan nama mahasiswa
